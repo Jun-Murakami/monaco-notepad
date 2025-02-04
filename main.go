f46d028b9442d71dffeee8f26a2da5936bc790fb
Jun-Murakami <contact@bucketrelay.com>
2025-02-04 10:29:20 +0900

    chore: add credential files to gitignore and set logging level

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
@@ -27,10 +28,11 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		OnBeforeClose:       app.BeforeClose,
+		OnBeforeClose:    app.BeforeClose,
 		Bind: []interface{}{
 			app,
 		},
+		LogLevel: logger.DEBUG,
 	})
 
 	if err != nil {
